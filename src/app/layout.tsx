4c50ac6b90eeef0b6c1378c55ab10324b40d6fa4
vishnu priya <vishnupriyamanikandan235@gmail.com>
2026-08-20 16:29:12 +0530

    error fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   description: "Employee Leave Management System",
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html
       lang="en"
